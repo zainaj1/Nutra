0f38d1ddfec516a73364ea66491516ce7b057b2f
zain_aj1 <zain_aj1@hotmail.com>
2026-05-02 14:12:38 -0400

    SCRUM-19: Implement auth flow with signin and signup pages

--- src/app/(app)/(auth)/sign-up.tsx
+++ src/app/(app)/(auth)/sign-up.tsx
@@ -22,12 +22,15 @@ export default function Page() {
         }
     }, [errors])
 
+    const [username, setUsername] = React.useState('')
     const [emailAddress, setEmailAddress] = React.useState('')
     const [password, setPassword] = React.useState('')
+
     const [code, setCode] = React.useState('')
 
     const handleSubmit = async () => {
         const { error } = await signUp.password({
+            username,
             emailAddress,
             password,
         })
@@ -51,6 +54,7 @@ export default function Page() {
                     // See https://clerk.com/docs/guides/development/custom-flows/authentication/session-tasks
                     if (session?.currentTask) {
                         console.log(session?.currentTask)
+                        console.log("Logging in here")
                         return
                     }
 
@@ -79,32 +83,69 @@ export default function Page() {
         signUp.missingFields.length === 0
     ) {
         return (
-            <SafeAreaProvider>
-                <Text>
-                    Verify your account
-                </Text>
-                <TextInput
-                    value={code}
-                    placeholder="Enter your verification code"
-                    placeholderTextColor="#666666"
-                    onChangeText={(code) => setCode(code)}
-                    keyboardType="numeric"
-                />
-                {errors.fields.code && (
-                    <Text>{errors.fields.code.message}</Text>
-                )}
-                <Pressable
-                    onPress={handleVerify}
-                    disabled={fetchStatus === 'fetching'}
-                >
-                    <Text>Verify</Text>
-                </Pressable>
-                <Pressable
-                    onPress={() => signUp.verifications.sendEmailCode()}
-                >
-                    <Text>I need a new code</Text>
-                </Pressable>
-            </SafeAreaProvider>
+            <SafeAreaProvider className="flex-1">
+                <View className="flex-1 px-6">
+                    <View className="flex-1 justify-center">
+                        {/* Logo */}
+                        <View className="items-center mb-6" style={{ flexShrink: 1 }}>
+                            <View className="w-20 h-15 bg-gradient-to- br from-blue-500 to-purple-600 rounded-2xl items-center justify-center mb-2 shadow-lg">
+                                <Ionicons name="restaurant-outline" size={iconSize} color="white" />
+                            </View>
+                            <Text className="text-3xl font-bold text-gray-900 mb-2">
+                                Join Nutra
+                            </Text>
+                            <Text className="text-lg text-gray-600 text-center">
+                                A nutrition and meal planning app{"\n"} to help you stay on track
+                            </Text>
+                        </View>
+
+                        {/* Header Section */}
+
+                        <KeyboardAvoidingView
+                            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                            className="bg-white rounded-2xl p-6 shadow-sm border border-gray-100 mb-6" style={{ flexShrink: 1 }}
+                        >
+                            <Text className="text-xl text-gray-900 font-bold text-center mb-4">
+                                Verify your account
+                            </Text>
+
+                            <View className="flex-row items-center bg-gray-50 rounded-xl border-gray-200 border px-4 py-4 mb-2">
+                                <Ionicons name="mail-outline" size={20} color="#6B7280" />
+                                <TextInput
+                                    value={code}
+                                    placeholder=" Enter your verification code"
+                                    placeholderTextColor="#666666"
+                                    onChangeText={(code) => setCode(code)}
+                                    keyboardType="numeric"
+                                />
+                                {errors.fields.code && (
+                                    <Text>{errors.fields.code.message}</Text>
+                                )}
+                            </View>
+                            <TouchableOpacity
+                                onPress={handleVerify}
+                                disabled={fetchStatus === 'fetching'}
+                                className={"rounded-xl py-4 px-4 shadow-sm mb-2 " + (isLoading ? "bg-gray-400" : "bg-blue-600")}
+                                activeOpacity={0.8}
+                            >
+                                <View className="flex-row items-center justify-center">
+                                    {isLoading ?
+                                        (<Ionicons name="refresh" size={20} color="white" className="animate-spin" />) :
+                                        (<Ionicons name="person-add-outline" size={20} color="white" />)}
+                                    <Text className='text-white font-semibold text-lg ml-2'>Verify</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <View className="flex-row items-center justify-center mb-6">
+                                <Pressable
+                                    onPress={() => signUp.verifications.sendEmailCode()}
+                                >
+                                    <Text className="text-blue-600 font-semibold">I need a new code</Text>
+                                </Pressable>
+                            </View>
+                        </KeyboardAvoidingView>
+                    </View>
+                </View >
+            </SafeAreaProvider >
         )
     }
     return (
@@ -137,6 +178,29 @@ export default function Page() {
                         <Text className="text-xl text-gray-900 font-bold text-center mb-4">
                             Create your account
                         </Text>
+
+                        {/* username Input  */}
+                        <Text className="text-sm font-medium text-gray-700 mb-2">
+                            username
+                        </Text>
+
+                        <View className="flex-row items-center bg-gray-50 rounded-xl border-gray-200 border px-4 py-4 mb-2">
+                            <Ionicons name="person-outline" size={20} color="#6B7280" />
+                            <TextInput
+                                autoCapitalize="none"
+                                value={username}
+                                placeholder="Create a username"
+                                placeholderTextColor="#666666"
+                                onChangeText={(username) => setUsername(username)}
+                                keyboardType="email-address"
+                                className="flex-1 ml-3 text-gray-900"
+                                editable={!isLoading}
+
+                            />
+                            {errors.fields.emailAddress && (
+                                <Text className="text-red-600 text-xs -mt-2">{errors.fields.emailAddress.message}</Text>
+                            )}
+                        </View>
 
                         {/* Email Input  */}
                         <Text className="text-sm font-medium text-gray-700 mb-2">
--- src/app/(app)/(tabs)/settings.tsx
+++ src/app/(app)/(tabs)/settings.tsx
@@ -1,9 +1,24 @@
-import { useAuth } from '@clerk/expo'
-import { ActivityIndicator, Text, View } from 'react-native'
+import { useAuth, useClerk } from '@clerk/expo'
+import { useRouter } from 'expo-router'
+import { ActivityIndicator, Pressable, Text, View } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 
 export default function Settings() {
     const { isSignedIn, isLoaded } = useAuth()
+    const { signOut } = useClerk()
+    const router = useRouter()
+    const handleSignOut = async () => {
+        try {
+            await signOut()
+            // Redirect to your desired page
+            router.replace('/')
+        } catch (err) {
+            // See https://clerk.com/docs/guides/development/custom-flows/error-handling
+            // for more info on error handling
+            console.error(JSON.stringify(err, null, 2))
+        }
+    }
 
     if (!isLoaded) {
         return (<View className="flex-1 items-center justify-center">
@@ -12,5 +27,22 @@ export default function Settings() {
         )
     }
 
-    return <Text>Settings</Text>
+    return (
+        <SafeAreaProvider className="flex-1">
+            <View className="flex-1 px-6">
+                {/* Header Section */}
+                <Text className="center-text px-6">Settings</Text>
+                <View className="flex-1 justify-center">
+
+                    <View>
+                        <Pressable
+                            onPress={handleSignOut}
+                        >
+                            <Text>Sign out</Text>
+                        </Pressable>
+                    </View>
+                </View >
+            </View>
+        </SafeAreaProvider >
+    )
 }
